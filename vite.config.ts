8d8fd4242d3ac8b88626e6d3deae80917e03c663
louisbrent1992 <louisbrent1992@gmail.com>
2025-06-15 11:58:01 -0700

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
+	publicDir: "public",
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+		emptyOutDir: true,
+	},
 });
